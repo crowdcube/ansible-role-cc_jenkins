043963044550fc1f5585f859debaf76a05c8d6cb
Nauris Sadovskis <nauris.sadovskis@crowdcube.com>
2017-09-14 16:15:56 +0100

    Add tests

--- test/integration/jenkins/serverspec/localhost/docker_spec.rb
+++ test/integration/jenkins/serverspec/localhost/docker_spec.rb
@@ -3,3 +3,7 @@ require 'spec_helper'
 describe service('docker') do
     it { should be_running }
 end
+
+describe command('curl -sL -w "%{http_code}\\n" "localhost:237" -o /dev/null | sed "s/[0-9]\+/OK/"') do
+    its(:stdout) { should match 'OK' }
+  end
